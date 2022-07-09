c3b0f84ee3f869b59fb728e3cbc1243a24b49673
Grizzly <medved1991alexander@gmail.com>
2022-07-09 15:37:52 +0300

    Added Homework 6

---
+++ CSharp.Homeworks/hw_06/HW.06.Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Data
+char oldChar = 'O';
+char newChar = 'A';
+
+//Input
+Console.WriteLine("Enter the poem on one line. Strings must be split \";\" symbol:");
+string? poem = Console.ReadLine();
+
+//Logic
+string[]? poemStr = poem?.Split(';');
+
+for (int s = 0; s < poemStr?.Length; s++)
+{
+    char[] chars = poemStr[s].ToCharArray();
+
+    for (int c = 0; c < chars.Length; c++)
+    {
+        if (chars[c].Equals(oldChar))
+        {
+            chars[c] = newChar;
+        }
+    }
+    string changeStr = new string(chars);
+
+    Console.WriteLine(changeStr);   
+}
