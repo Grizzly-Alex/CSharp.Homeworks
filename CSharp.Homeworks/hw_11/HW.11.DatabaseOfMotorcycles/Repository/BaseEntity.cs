90be694e44e7fd5b5ecea1efe2f7067f8b2a775f
Grizzly <medved1991alexander@gmail.com>
2022-07-24 22:57:31 +0300

    Added Homework 11

---
+++ CSharp.Homeworks/hw_11/HW.11.DatabaseOfMotorcycles/Repository/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._11.DatabaseOfMotorcycles.Repository
+{
+    internal abstract class BaseEntity
+    {
+        protected virtual Guid Id { get; set; }
+
+        public abstract Guid GetId();
+    }
+}
---
+++ CSharp.Homeworks/hw_11/HW.11.DatabaseOfMotorcycles/Repository/Motorcycle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace HW._11.DatabaseOfMotorcycles.Repository
+{
+    internal class Motorcycle: BaseEntity
+    {
+        protected override Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometer { get; set; }
+        
+        public Motorcycle(string name, string model)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Model = model;
+            Year = int.Parse(DateTime.Now.ToString("yyyy"));
+            Odometer = default;
+            Log.Information($"Motorcycle created: {ToString()}");
+        }
+
+  
+        public override string ToString()
+        {
+            return ($"Name: {Name}, Model: {Model}, Year: {Year}, Odometer: {Odometer}km, Id: {Id}");
+        }
+
+        public override Guid GetId()
+        {
+            return Id;
+        }
+
+    }
+}
